962c8b4fdf6c066216fb1b28650608758e1c0fa6
ym <yael.montufar@protonmail.com>
2022-04-28 16:12:46 -0500

    Configure absolute paths for src directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GlobalStyles from './styles/GlobalStyles';
+import { GlobalStyles } from 'styles';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
