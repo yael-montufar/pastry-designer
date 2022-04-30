d7a715f0dfea6bf4c6dea393cf5a5892c001c8a8
ym <yael.montufar@protonmail.com>
2022-04-30 12:34:55 -0500

    Implement Canvas SVG output for donut-glazings
    All SVG components were made position absolute.
    donut-glazings SVG library was added to ./src/assets.
    SVG styles were applied to remove a spare layer left from what could be a poor asset export.

--- src/templates/Canvas.jsx
+++ src/templates/Canvas.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import 'styled-components/macro'
 import tw, { css } from 'twin.macro'
 import { svgSelector } from 'helpers'
-import { DonutTypes } from 'assets'
+import { DonutTypes, DonutGlazings } from 'assets'
 import { theme } from 'styles'
 
 
@@ -14,6 +14,16 @@ const Canvas = ({ donutType, donutFlavor, donutGlazings, donutToppings }) => {
       <div css={styles.donutTypeOutput({ fill: theme.colors.flavors[donutFlavor] })}>
         <DonutTypeOutput className="donut-type-output" />
       </div>
+
+      {donutGlazings.map((donutGlazing) => {
+        const DonutGlazingOutput = svgSelector(DonutGlazings, donutGlazing)
+
+        return (
+          <div css={styles.donutGlazingOutput}>
+            <DonutGlazingOutput className="donut-glazing-output" />
+          </div>
+        )
+      })}
     </div>
   )
 }
@@ -36,6 +46,7 @@ const styles = {
   ],
   donutTypeOutput: ({ fill }) => [
     tw`
+    absolute
     w-full
     rounded rounded-full
     `,
@@ -44,8 +55,21 @@ const styles = {
     svg.donut-type-output #MOD{
       fill: ${fill} !important; //override style prop if present
     };
-    svg path.st5 { //cruller_base.svg inner circle fill color
+    svg.donut-type-output path.st5 { //cruller_base.svg inner circle fill color
       fill: ${theme.colors.background};
+    };
+    `,
+  ],
+  donutGlazingOutput: () => [
+    tw`
+    absolute
+    w-full
+    rounded rounded-full
+    `,
+
+    css`
+    svg.donut-glazing-output .st10 { //remove Layer_3 (sprinkles) group - Asset Export Error
+      display: none;
     };
     `,
   ],
